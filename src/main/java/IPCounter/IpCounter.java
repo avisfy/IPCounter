2ce9e1821eb901ff9d74abc96ca6d0fedd9cd3b9
avisfy <avisfy@mail.ru>
2020-03-19 16:51:15 +0400

    small fixes

--- src/main/java/IPCounter/IpCounter.java
+++ src/main/java/IPCounter/IpCounter.java
@@ -1,46 +1,60 @@
-package IpCounter;
+package IPCounter;
 
-import java.io.BufferedReader;
-        import java.io.FileInputStream;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.util.Scanner;
+import java.io.*;
+import java.util.Scanner;
 
 public class IpCounter {
 
     private static byte[][][][] addrArr;
 
+
     public static void main(String[] args) {
         System.out.println("Enter filename:");
-        System.out.print("> ");
+        //System.out.print("> ");
         Scanner in = new Scanner(System.in);
         String filename = in.nextLine();
-
-        try (BufferedReader freader = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))) {
-            String addrLine;
+        long k = 0;
+        try (BufferedReader freader = new BufferedReader(new InputStreamReader(new FileInputStream(new File(filename))))) {
             addrArr = new byte[256][][][];
-            int k = 0;
+            String addrLine;
             while ((addrLine = freader.readLine()) != null) {
-                if (!addrLine.isEmpty())
+                if (!addrLine.isEmpty()) {
                     k = k + findAddr(addrLine);
+                    //if (k % 1000000 == 0) {
+                        //System.out.println("working: " + addrLine + " " + k);
+                    //}
+                }
             }
             System.out.println("Unique: " + k);
         } catch (NumberFormatException e) {
             System.out.println("Address format error");
         } catch (IOException e) {
             System.out.println("Reading file error");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error:" +  k);
         }
     }
 
-    private static int findAddr(String addrStr) throws NumberFormatException {
+
+    private static int findAddr(String addrStr) throws Exception {
         int pos1, pos2, pos3, pos4;
         int k = 0;
-        String[] strArr = addrStr.split("\\.");
+        int posPoint1= 0;
+        int posPoint2= -1;
+        String[] strArr = new String[4];
+        for (int i = 0; i<4; i++) {
+            posPoint1 = posPoint2 + 1;
+            posPoint2 = addrStr.indexOf('.', posPoint1);
+            if (posPoint2 != -1)
+                strArr[i] = new String(addrStr.substring(posPoint1, posPoint2));
+            else
+                strArr[i] = new String(addrStr.substring(posPoint1));
+        }
         pos1 = Integer.parseInt(strArr[0]);
         pos2 = Integer.parseInt(strArr[1]);
         pos3 = Integer.parseInt(strArr[2]);
         pos4 = Integer.parseInt(strArr[3]);
-        System.out.println(addrStr + "\t" + pos1 + " " + pos2 + " " + pos3 + " " + pos4);
         if (addrArr[pos1] == null) {
             addrArr[pos1] = new byte[256][][];
             addrArr[pos1][pos2] = new byte[256][];
@@ -64,6 +78,7 @@ public class IpCounter {
                 k--;
             }
         }
+        addrStr = null;
         return k;
     }
 }
